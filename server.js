ba1899139d884b295b58bf54ca1934cb31469d38
Ayush Kumar Jha <ayush91176@gmail.com>
2022-12-27 23:02:52 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = process.env.PORT ||8000 
+
+http.listen(PORT,()=>{
+    console.log('Listening on port ${PORT}')
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname +'/public/index.html')
+})
+
+// Socket
+
+const io = require('socket.io')(http)
+io.on('connection',(socket)=>{
+    console.log('Connect.....')
+    socket.on('message',(msg)=>{
+        socket.broadcast.emit('message',msg)
+    })
+})
